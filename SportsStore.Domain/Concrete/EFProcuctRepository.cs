09a4b85de68d3f4b66fea8d9f36770a5e7088c9d
Samuel Lane <samuel.lane.sa@outlook.com>
2019-05-11 20:32:40 -0400

    Add project files.

---
+++ SportsStore.Domain/Concrete/EFProcuctRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.Domain.Concrete
+{
+    class EFProcuctRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}

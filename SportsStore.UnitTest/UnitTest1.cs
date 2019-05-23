878d79d88fd69b52860580e7204da7484e9e6acb
Samuel Lane <samuel.lane.sa@outlook.com>
2019-05-23 09:49:10 -0400

    SportsStoreLab3D

--- SportsStore.UnitTest/UnitTest1.cs
+++ SportsStore.UnitTest/UnitTest1.cs
@@ -14,6 +14,7 @@ namespace SportsStore.UnitTests
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
         public void Can_Send_Pagination_View_Model()
         {
@@ -86,6 +87,68 @@ namespace SportsStore.UnitTests
             + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
             + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
             result.ToString());
+        }
+
+        [TestClass]
+        public class CartTests
+        {
+            [TestMethod]
+            public void Can_Add_New_Lines()
+            {
+                // Arrange - create some test products
+                Product p1 = new Product { ProductID = 1, Name = "P1" };
+                Product p2 = new Product { ProductID = 2, Name = "P2" };
+                // Arrange - create a new cart
+                Cart target = new Cart();
+                // Act
+                target.AddItem(p1, 1);
+                target.AddItem(p2, 1);
+                CartLine[] results = target.Lines.ToArray();
+                // Assert
+                Assert.AreEqual(results.Length, 2);
+                Assert.AreEqual(results[0].Product, p1);
+                Assert.AreEqual(results[1].Product, p2);
+            }
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            // Arrange - create a new cart
+            Cart target = new Cart();
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+            // Assert
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Quantity, 11);
+            Assert.AreEqual(results[1].Quantity, 1);
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+            // Arrange - create a new cart
+            Cart target = new Cart();
+            // Arrange - add some products to the cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+            // Act
+            target.RemoveLine(p2);
+            // Assert
+            Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
         }
     }
 }
---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                ReturnUrl = returnUrl,
+                Cart = cart
+            });
+        }
+        public RedirectToRouteResult AddToCart(Cart cart, int productId,
+        string returnUrl)
+        {
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId,
+        string returnUrl)
+        {
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+    }
+}
